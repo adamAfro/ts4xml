6a267c29e7a3ba261e196c62a73fc2774e805f9f
Adam Jakubczak <adam.per.jakubczak@gmail.com>
2024-07-08 23:50:56 +0200

    poprawka w typach prostych i ograniczonym enum atrybuty sa ograniczone tylko do pewnych typów, values też było zbędne w typie Property

--- parser.ts
+++ parser.ts
@@ -2,13 +2,13 @@ import { createProgram, Node as TypescriptNode, SyntaxKind } from "https://esm.s
 
 interface Element { name: string, properties: Property[] }
 type SimpleType = 'any'|'boolean'|'number'|'string'|'null'|'undefined'
-type RestrictedType = { value: any[] }
+type RestrictedType = { value: number|string }
 type ReferenceType = { reference: string }
+type AnyType = SimpleType|RestrictedType|ReferenceType
 interface Property { 
     name: string,
     mandatory: boolean,
-    types: (SimpleType|RestrictedType|ReferenceType)[],
-    value?: any[],
+    types: (SimpleType|RestrictedType|ReferenceType)[]
     multiple?: boolean
 }
 
